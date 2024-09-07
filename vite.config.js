8c5546ea4dcfe637fb9a706c8f3f5350ba363a52
Guillaume <101353118+cordellier@users.noreply.github.com>
2024-09-07 10:24:32 +0200

    Importation typographie

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@styles": path.resolve(__dirname, "src/assets/styles"),
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "./src/assets/styles/abstracts/variables";`,
+        additionalData: `@import "@styles/abstracts/variables.scss"; @import "@styles/base/typography.scss";`,
       },
     },
   },
